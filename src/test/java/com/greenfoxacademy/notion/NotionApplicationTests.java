e1edf35aacc1b98149634bc159fd03f3925fe57d
merlinbergmann <hello@merlin.codes>
2018-01-16 13:21:06 +0100

    init(notion: A Reddit copycat)

---
+++ src/test/java/com/greenfoxacademy/notion/NotionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.notion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
