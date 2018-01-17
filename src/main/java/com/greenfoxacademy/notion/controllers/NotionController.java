55c454ec152eed75c5381d2073eac9cb6739c5af
merlinbergmann <hello@merlin.codes>
2018-01-17 16:23:20 +0100

    postRequest for notion creation added

--- src/main/java/com/greenfoxacademy/notion/controllers/NotionController.java
+++ src/main/java/com/greenfoxacademy/notion/controllers/NotionController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -29,5 +31,11 @@ public class NotionController {
   public String showAddPage(Model model) {
     model.addAttribute("newNotion", notionFactory.getEmptyNotion());
     return "addnotion";
+  }
+
+  @PostMapping("/add")
+  public String addNotion(@ModelAttribute Notion notion) {
+    notionService.addNotion(notion);
+    return "redirect:";
   }
 }
